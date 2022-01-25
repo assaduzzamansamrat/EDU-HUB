1ec67b247373b3ea927717e3aa33f22c959df3c7
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2022-01-25 16:39:52 +0600

    Update

---
+++ Services/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Services.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+      
+
+    }
+}
